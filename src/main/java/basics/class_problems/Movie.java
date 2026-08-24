4e43057033f2ca3871ffc3b5138dd37d1beee664
archi-sys1 <mazumderarchisha@gmail.com>
2026-08-24 22:38:08 +0530

    Add Week 1 problems

---
+++ src/main/java/basics/class_problems/Movie.java
@@ -0,0 +1,22 @@
+package Step_semester_3.src.main.java.basics.class_problems;
+class Movie {
+    static void check(String s) {
+        String[] w = s.split(" ");
+        int sh = 0, me = 0, lo = 0;
+
+        for (String x : w) {
+            if (x.length() <= 4)
+                sh++;
+            else if (x.length() <= 8)
+                me++;
+            else
+                lo++;
+        }
+
+        System.out.println("Short: " + sh +
+                " | Medium: " + me + " | Long: " + lo);
+    }
+
+    public static void main(String[] args) {
+        check("This movie was absolutely fantastic and thrilling");
+    }

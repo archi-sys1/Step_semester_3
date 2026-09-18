20b36ce06d4d70b2b4a404b506563891fc32b17a
archi-sys1 <mazumderarchisha@gmail.com>
2026-09-18 23:38:39 +0530

    Add Session 6 assignment problems

---
+++ src/main/java/basics/assigment_problems/Q5.java
@@ -0,0 +1,29 @@
+package Step_semester_3.src.main.java.basics.assigment_problems;
+
+class Employee {
+    String empName;
+    double salary;
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+}
+
+public class Q5{
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Divya", 65000);
+        Employee e2 = new Employee("Arjun", 55000);
+        Employee e3 = new Employee("Ravi", 60000);
+
+        Employee.printCompanyInfo();
+    }
+}
---
+++ src/main/java/basics/assigment_problems/q2.java
@@ -0,0 +1,48 @@
+package Step_semester_3.src.main.java.basics.assigment_problems;
+class PayrollAccount {
+    private double basicSalary;
+    private double bonus;
+
+    public PayrollAccount(double basicSalary) {
+        if (basicSalary < 0) {
+            System.out.println("Warning: Negative salary not allowed");
+            this.basicSalary = 0;
+        } else {
+            this.basicSalary = basicSalary;
+        }
+        this.bonus = 0;
+    }
+
+    public void creditBonus(double amount) {
+        if (amount <= 0) {
+            System.out.println("Bonus rejected");
+        } else {
+            bonus += amount;
+            System.out.println("Bonus credited: Rs " + amount);
+        }
+    }
+
+    public void deductTax(double percent) {
+        if (percent < 0 || percent > 100) {
+            System.out.println("Invalid tax percentage");
+        } else {
+            basicSalary -= basicSalary * percent / 100;
+            System.out.println("Tax deducted: " + percent + "%");
+        }
+    }
+
+    public double getNetSalary() {
+        return basicSalary + bonus;
+    }
+}
+
+public class q2 {
+    public static void main(String[] args) {
+        PayrollAccount account = new PayrollAccount(50000);
+
+        account.creditBonus(5000);
+        account.deductTax(10);
+
+        System.out.println("Net salary: Rs " + account.getNetSalary());
+    }
+}
